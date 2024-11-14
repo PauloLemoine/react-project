becec8cf92b6f8272babcc60608778da672d7651
Paulo Lemoine <paulolemoine19@gmail.com>
2024-11-14 03:27:40 -0300

    atualização nas rotas

--- minha-aplicacao-react/src/App.tsx
+++ minha-aplicacao-react/src/App.tsx
@@ -15,6 +15,7 @@ const App: React.FC = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Login />} />
       </Routes>
     </Suspense>
     </BrowserRouter>
